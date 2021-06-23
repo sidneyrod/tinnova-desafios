8ff3667c94fce6f7190e5e52d0757b5273003f94
Sidney Rodrigues <sidney.silva.rodrigues@outlook.com>
2021-06-23 16:25:38 -0300

    Atualizando ex-1

--- exercicio-um/src/application/Program.java
+++ exercicio-um/src/application/Program.java
@@ -6,15 +6,15 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		double totalEleitores = 1000;
-		double votosValidos = 800;
-		double votosBrancos = 150;
-		double votosNulos = 50;
+		Votos votos = new Votos();
 		
-		Votos votos = new Votos(totalEleitores, votosValidos, votosBrancos, votosNulos);
+		votos.setTotalEleitores(1000.0);
+		votos.setVotosValidos(800.0);
+		votos.setVotosBrancos(150.0);
+		votos.setVotosNulos(50.0);
 		
-		System.out.println("Porcentagem de votos válidos: " + votos.percentualVotosValidos(votosValidos) + "%");
-		System.out.println("Porcentagem de votos brancos: " + votos.percentualVotosBrancos(votosBrancos) + "%");
-		System.out.println("Porcentagem de votos nulos: " + votos.percentualVotosNulos(votosNulos) + "%");
+		System.out.println("Porcentagem de votos válidos: " + votos.percentualVotosValidos(votos.getVotosValidos()) + "%");
+		System.out.println("Porcentagem de votos brancos: " + votos.percentualVotosBrancos(votos.getVotosBrancos()) + "%");
+		System.out.println("Porcentagem de votos nulos: " + votos.percentualVotosNulos(votos.getVotosNulos()) + "%");
 	}
 }
